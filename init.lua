d40c1e8baaef872365d6d8e9d376c6992418e1f7
Sergey Zorin <sergey@zorin.ru>
2019-03-05 17:35:02 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,21 @@
+enable_example01 = true
+enable_example02 = true
+
+dofile("plugins/CommonParser/CommonParser.lua_")
+
+tcp_table = DissectorTable.get ("tcp.port") 
+
+
+if( enable_example01  ) then
+	dofile("plugins/CommonParser/example01.lua_")
+	tcp_table:add (34501, example01_proto)
+end
+
+if( enable_example02 ) then
+	dofile("plugins/CommonParser/example02.lua_")
+	tcp_table:add (34502, example01_proto)
+end
+
+
+
+
